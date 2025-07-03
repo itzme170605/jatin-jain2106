2f3c8019352c9e9f65360c8af1c7e84e5a56a5da
Jatin <j2130132806@gmail.com>
2025-07-03 08:17:59 -0400

    added moon texture and normal maps

--- src/components/Moon3D.tsx
+++ src/components/Moon3D.tsx
@@ -9,8 +9,8 @@ export default function Moon3D() {
   return (
     <div className="fixed top-0 left-0 w-1/2 h-screen z-0">
       <Canvas camera={{ position: [0, 0, 3] }}>
-        <ambientLight intensity={0.1} />
-        <directionalLight position={[2, 2, 5]} intensity={1} />
+        <ambientLight intensity={0.2} />
+        <directionalLight position={[2, 2, 5]} intensity={1.2} />
         <RotatingMoon />
       </Canvas>
     </div>
@@ -19,7 +19,11 @@ export default function Moon3D() {
 
 function RotatingMoon() {
   const meshRef = useRef<THREE.Mesh>(null);
-  const texture = useLoader(TextureLoader, '/moon_texture.jpg'); // We'll add this file next
+
+  const [colorMap, normalMap] = useLoader(TextureLoader, [
+    '/moon_texture.jpg',
+    '/moon_normal_8k.png',
+  ]);
 
   useFrame(({ clock }) => {
     if (meshRef.current) {
@@ -29,8 +33,13 @@ function RotatingMoon() {
 
   return (
     <mesh ref={meshRef}>
-      <sphereGeometry args={[0.11,128, 128]} />
-      <meshStandardMaterial map={texture} />
+      <sphereGeometry args={[1, 128, 128]} />
+      <meshStandardMaterial
+        map={colorMap}
+        normalMap={normalMap}
+        metalness={0.2}
+        roughness={1}
+      />
     </mesh>
   );
 }
